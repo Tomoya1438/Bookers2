8cff4d95cdd530b57ac621738b2dcfadbf821ffd
EC2 Default User <ec2-user@ip-172-31-26-230.ap-northeast-1.compute.internal>
2024-11-01 15:58:24 +0000

    1

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -8,15 +8,17 @@ class BooksController < ApplicationController
       flash[:notice] = "You have created book successfully."
       redirect_to book_path(@book.id)
     else
-       @books = Book.all
-       render :index
+      @books = Book.all
+      @user = current_user
+      render :index
     end
   end
 
   def index
-    @book = Book.new
     @books = Book.all
     @user = current_user
+    @book = Book.new
+    
   end
 
   def show
@@ -54,9 +56,9 @@ class BooksController < ApplicationController
   end
 
   def is_matching_login_user
-    user = Book.find(params[:id])
-    unless user.id == current_user.id
-      redirect_to current_user
+    book = Book.find(params[:id])
+    unless book.user.id == current_user.id
+      redirect_to books_path
     end
   end
 
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,29 +1,29 @@
 class UsersController < ApplicationController
   before_action :is_matching_login_user, only: [:edit, :update]
-  
+
   def index
     @users = User.all
     @book_new = Book.new
     @user = current_user
   end
-  
+
   def create
     @book = Book.new(book_params)
     @book.user_id = current_user.id
     @book.save
     redirect_to book_path(@book.id)
   end
-  
+
   def show
     @book_new = Book.new
     @user = User.find(params[:id])
-    @books = @user.books 
+    @books = @user.books
   end
 
   def edit
     @user = User.find(params[:id])
   end
-  
+
   def update
     @user = User.find(params[:id])
     if @user.update(user_params)
@@ -34,13 +34,13 @@ class UsersController < ApplicationController
        render :edit
     end
   end
-  
+
   private
-  
+
   def user_params
     params.require(:user).permit(:name, :introduction, :profile_image)
   end
-  
+
   def is_matching_login_user
     user = User.find(params[:id])
     unless user.id == current_user.id
